8bcd5489668963d90e2f79bc12130f37c1bccdf3
Nicolas <nico03aguirre@gmail.com>
2023-09-26 08:47:48 -0600

    added auth0 authorization

---
+++ backend/auth0/auth0-config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  };
