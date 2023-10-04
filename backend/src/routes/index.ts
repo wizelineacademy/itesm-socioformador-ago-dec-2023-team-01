ebba96ba76ef83edf6773bb4a5d7bbf3be7811e9
Pablo <pabloerhard02@gmail.com>
2023-10-04 09:03:12 -0600

    added index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.info(req.oidc.isAuthenticated());
-  console.info('user:', req.oidc.user);
+  console.log(req.oidc.isAuthenticated());
+  console.log('user:', req.oidc.user);
   res.render('index', {
     title: 'Express',
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
   });
 });
+
 
 export default router;
