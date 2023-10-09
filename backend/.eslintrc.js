2611e4070d2a767d915e479b1589b88a89485811
Nicolas <nico03aguirre@gmail.com>
2023-10-09 09:49:32 -0600

    added config to eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     'import/extensions': ['error', 'ignorePackages', { ts: 'never' }],
     'no-underscore-dangle': ['error', { allow: ['_count'] }],
-    'no-console': ['error', { allow: ['info', 'error'] }],
+    // 'no-console': ['error', { allow: ['info', 'error'] }],
     'import/no-extraneous-dependencies': ['error', { devDependencies: ['tests/prisma-mock.ts'] }],
     semi: ['error', 'always'],
   },
