304d2b9504210536da0120a1407f227ae4b8920f
Nicolas <nico03aguirre@gmail.com>
2023-10-08 22:34:08 -0600

    added eslint to project and fixed eslint problems

--- frontend/src/app/components/TWButton.tsx
+++ frontend/src/app/components/TWButton.tsx
@@ -1,20 +1,23 @@
+import React from 'react';
 import { Button } from '@mui/material';
 
 interface ButtonProps {
-    text: String;
+  text: String;
 }
 
-export default function WButton({text}:ButtonProps) {
-    return(
-        <Button
-            variant='contained'
-            size='large'
-            sx={{
-                bgcolor: 'white',
-                color: 'gray',
-                "&:hover": {bgcolor: 'gray', color: "white"},
-                padding: '0.7rem 2rem'
-            }}
-        >{text}</Button>
-    );
+export default function WButton({ text }: ButtonProps) {
+  return (
+    <Button
+      variant="contained"
+      size="large"
+      sx={{
+        bgcolor: 'white',
+        color: 'gray',
+        '&:hover': { bgcolor: 'gray', color: 'white' },
+        padding: '0.7rem 2rem',
+      }}
+    >
+      {text}
+    </Button>
+  );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,35 +1,49 @@
-"use client"
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import styles from './page.module.css';
-import TWButton from './components/TWButton';
-import WTitle1 from './components/WTitle1';
-
+"use client";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import styles from "./page.module.css";
+import TWButton from "./components/TWButton";
+import WTitle1 from "./components/WTitle1";
 
 export default function Home() {
   return (
     <Grid container spacing={0} className={styles.container}>
       <Grid xs={12} md={6} className={styles.container}>
-          <div className={`${styles.center}`}>
-              <WTitle1 text={'Welcome to WIZE'} redText={'PROMPT.'}/>
-              <TWButton text={'Sign In with Google'}/>
-              <div className={styles.spaceBetween}>
-                  <Grid container spacing={0}>
-                      <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
-                          <img src="./chatgpt.png" className={styles.smallImage}/>
-                      </Grid>
-                      <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
-                          <img src="./Bard.png" className={styles.smallImage}/>
-                      </Grid>
-                      <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
-                          <img src="./llama2.png" className={styles.smallImage}/>
-                      </Grid>
-                  </Grid>
-              </div>
+        <div className={`${styles.center}`}>
+          <WTitle1 text={"Welcome to WIZE"} redText={"PROMPT."} />
+          <TWButton text={"Sign In with Google"} />
+          <div className={styles.spaceBetween}>
+            <Grid container spacing={0}>
+              <Grid
+                xs={4}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <img src="./chatgpt.png" className={styles.smallImage} />
+              </Grid>
+              <Grid
+                xs={4}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <img src="./Bard.png" className={styles.smallImage} />
+              </Grid>
+              <Grid
+                xs={4}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <img src="./llama2.png" className={styles.smallImage} />
+              </Grid>
+            </Grid>
           </div>
+        </div>
       </Grid>
       <Grid xs={0} md={6}>
-          <img src="./inspiration.jpg" className={styles.image}/>
+        <img src="./inspiration.jpg" className={styles.image} />
       </Grid>
-      </Grid>
-  )
+    </Grid>
+  );
 }
