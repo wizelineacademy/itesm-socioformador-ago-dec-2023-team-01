f90b457c418d91bc43aa4f03b0c55e51379ece7a
Pablo <pabloerhard02@gmail.com>
2023-10-10 10:31:22 -0600

    feature: prisma and services restructure

--- backend/src/services/groupServices.ts
+++ backend/src/services/groupServices.ts
@@ -1,21 +1,56 @@
-
 import prisma from '../../prisma/prisma-client';
 
 
-export async function createGroup(name: string) {
-    const group = await prisma.group.create({
+// Create group
+export async function createGroup(name: string, area: string) {
+    return prisma.group.create({
         data: {
             name,
+            area,
         }
     });
-    return group;
 }
 
-export async function deleteGroup(id: string) {
-    const group = await prisma.group.delete({
+// Delete Group
+export async function deleteGroup(id: number) {
+    return prisma.group.delete({
         where: {
             id,
         }
     });
-    return group;
+}
+
+// Updates
+export async function addUserToGroupById(id: string, groupId: number) {
+    try {
+        const user = await prisma.user.findUnique({where: {id}});
+        const group = await prisma.group.findUnique({where: {id: groupId}});
+        if (!user || !group) {
+            console.info('user not found');
+            return;
+        }
+        // eslint-disable-next-line consistent-return
+        return prisma.group.update({
+            where: {id: groupId},
+            data: {
+                users: {
+                    connect: {id}
+                }
+            }
+        });
+    } catch (e) {
+        console.info('Error', e);
+        throw e;
+    }
+}
+
+export async function removeUserFromGroup(userId: string, groupId: number) {
+    return prisma.group.update({
+        where: {id: groupId},
+        data: {
+            users: {
+                disconnect: {id: userId}
+            }
+        }
+    });
 }
