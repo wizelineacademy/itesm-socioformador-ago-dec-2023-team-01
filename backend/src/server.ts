05a8f59e34c2d12962c1c6e1de3edf2ca3e5aa9c
Nicolas <nico03aguirre@gmail.com>
2023-10-12 08:59:21 -0600

    feature: added swagger to web api backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,24 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { auth } from 'express-openid-connect';
 import config from './configs/auth0-config';
 import router from './routes/index';
-
+import swaggerDocument from './swagger_output.json';
 
 
 const port = 8080;
 const app = express();
 
 app.use(auth(config));
-app.use('/', router);
-
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.use('/', router);
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}`);
