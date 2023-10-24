d8bad1ec7a235ed37096a77fa4f8985c1121961d
Nicolas <nico03aguirre@gmail.com>
2023-10-24 08:26:36 -0600

    fix: thanosed unused files

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import userRouter from '../modules/user/userController';
-import loginRouter from '../controllers/loginController';
 import groupRouter from '../modules/group/groupController';
 import rolesRouter from '../modules/role/roleController';
 
@@ -8,7 +7,6 @@ const router = express.Router();
 
 router.use('/group', groupRouter);
 router.use('/users/', userRouter);
-router.use('/login', loginRouter);
 router.use('/roles/', rolesRouter);
 
 export default router;
