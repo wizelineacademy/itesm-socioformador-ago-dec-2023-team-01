ed40114addf76bca9fbff9155e01d506995a3256
Nicolas <nico03aguirre@gmail.com>
2023-10-28 19:23:11 -0600

    feat: auth endpoint & middleware for routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,10 +3,14 @@ import userRouter from '../modules/user/userController';
 import groupRouter from '../modules/group/groupController';
 import rolesRouter from '../modules/role/roleController';
 
+import authRouter from '../modules/auth/authController';
+import checkAuthorization from '../middlewares/authMiddleware';
+
 const router = express.Router();
 
-router.use('/group', groupRouter);
-router.use('/users/', userRouter);
-router.use('/roles/', rolesRouter);
+router.use('/auth/', authRouter);
+router.use('/group/', checkAuthorization, groupRouter);
+router.use('/users/', checkAuthorization, userRouter);
+router.use('/roles/', checkAuthorization, rolesRouter);
 
 export default router;
