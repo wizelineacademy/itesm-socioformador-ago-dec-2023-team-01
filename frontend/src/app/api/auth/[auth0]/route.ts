102b9a82231e90a0df5a62dac742008afd6cda81
Pablo <pabloerhard02@gmail.com>
2023-10-30 10:08:29 -0600

    added custom auth0 login

--- frontend/src/app/api/auth/[auth0]/route.ts
+++ frontend/src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,12 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    authorizationParams: {
+      audience: process.env.AUTH0_AUDIENCE,
+      scope: 'openid profile email',
+    },
+  }),
+});
+
+export default GET;
