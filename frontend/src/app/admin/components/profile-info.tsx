9a3a43c57998c767820811b6cf1e8d3b19572683
SamAcosta02 <a00833547@tec.mx>
2023-11-07 11:48:08 -0600

    fixed a bug

--- frontend/src/app/admin/components/profile-info.tsx
+++ frontend/src/app/admin/components/profile-info.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+
 'use client';
 
 import React from 'react';
@@ -7,48 +9,57 @@ import {
   Typography,
   Stack,
 } from '@mui/material';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { WelcomeProps } from '@/app/components/types';
 import styles from './sidenav.module.css';
+import Awaiting from '@/app/components/awaiting';
 
 export default function ProfileInfo({
   firstName,
   lastName,
   wizecoins,
 }: WelcomeProps) {
-  return (
-    <Box
-      sx={{
-        width: 'calc(100vw - 16.1rem)',
-        paddingTop: '1rem',
-      }}
-    >
-      <Grid container justifyContent="flex-end">
-        <Grid>
-          <Typography
-            sx={{ fontStyle: 'bold', color: 'white', fontSize: '25px' }}
-          >
-            {`${firstName} ${lastName}`}
-          </Typography>
-          <Stack
-            gap={0}
-            direction="row"
-            alignItems="center"
-            justifyContent="flex-end"
-          >
-            <object
-              data="/wizecoin.svg"
-              className={styles.smallimage}
-              title="wizecoin"
-            />
-            <Typography sx={{ color: '#4BE93D', fontSize: '25px' }}>
-              {wizecoins}
+  const { user, error } = useUser();
+
+  if (error) return <div>{error.message}</div>;
+  const pic:any = user?.picture;
+  if (user) {
+    console.log(user);
+    return (
+      <Box
+        sx={{
+          width: 'calc(100vw - 16.1rem)',
+          paddingTop: '1rem',
+        }}
+      >
+        <Grid container justifyContent="flex-end">
+          <Grid>
+            <Typography
+              sx={{ fontStyle: 'bold', color: 'white', fontSize: '25px' }}
+            >
+              {`${user.given_name} ${user.family_name}`}
             </Typography>
-          </Stack>
+            <Stack
+              gap={0}
+              direction="row"
+              alignItems="center"
+              justifyContent="flex-end"
+            >
+              <object
+                data="/wizecoin.svg"
+                className={styles.smallimage}
+                title="wizecoin"
+              />
+              <Typography sx={{ color: '#4BE93D', fontSize: '25px' }}>
+                {wizecoins}
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid paddingLeft="1rem">
+            <img src={pic} className={styles.images} alt="" />
+          </Grid>
         </Grid>
-        <Grid paddingLeft="1rem">
-          <img src="/tommy.png" className={styles.images} alt="" />
-        </Grid>
-      </Grid>
-    </Box>
-  );
+      </Box>
+    );
+  }
 }
