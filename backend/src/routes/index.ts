ba3c1f9c71e8c4cf400c659f197c585ad32b8475
Nicolas <nico03aguirre@gmail.com>
2023-11-07 13:01:48 -0600

    Add new routes for token, conversation, and language controllers

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+
 import userRouter from '../modules/user/userController';
 import groupRouter from '../modules/group/groupController';
 import rolesRouter from '../modules/role/roleController';
-
+import tokenRouter from '../modules/token/tokenController';
+import conversationRouter from '../modules/conversation/conversationController';
 import authRouter from '../modules/auth/authController';
+import languageRouter from '../modules/language/languageController';
+
 import checkAuthorizationMiddleware from '../middlewares/authMiddleware';
 
 const router = express.Router();
@@ -12,5 +16,8 @@ router.use('/auth/', authRouter);
 router.use('/group/', checkAuthorizationMiddleware, groupRouter);
 router.use('/users/', userRouter);
 router.use('/roles/', checkAuthorizationMiddleware, rolesRouter);
+router.use('/tokens/', tokenRouter);
+router.use('/conversations/', conversationRouter);
+router.use('/languages/', languageRouter);
 
 export default router;
