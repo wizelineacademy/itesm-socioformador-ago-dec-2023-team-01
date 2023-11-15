a6ed270f4e122036a8760da0162e172bf99be32e
diegoTec01 <116265019+diegoTec01@users.noreply.github.com>
2023-11-15 10:49:39 -0600

    feat: Logout button in Admin navbar.

--- frontend/src/app/admin/components/profile-info.tsx
+++ frontend/src/app/admin/components/profile-info.tsx
@@ -6,8 +6,13 @@ import {
   Grid,
   Typography,
   Stack,
+  Button,
 } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 import styles from './sidenav.module.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 function ProfileInfo({
   firstName,
@@ -35,6 +40,23 @@ function ProfileInfo({
             alignItems="center"
             justifyContent="flex-end"
           >
+            <Grid paddingRight="10rem">
+              <Button
+                style={{
+                  textTransform: 'none', // Set textTransform to 'none' to prevent all caps
+                }}
+                className={`${inter.className}`}
+                startIcon={<LogoutIcon />}
+              >
+                <Typography
+                  sx={{ fontStyle: 'bold', color: 'white', fontSize: '18px' }}
+                >
+                  <span>Logout</span>
+                </Typography>
+              </Button>
+
+            </Grid>
+
             <object
               data="/wizecoin.svg"
               className={styles.smallimage}
