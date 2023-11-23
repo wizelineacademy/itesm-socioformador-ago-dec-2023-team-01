09a6642e65ad7df49724d8160c10d33cc5e37fdb
Nicolas <nico03aguirre@gmail.com>
2023-11-22 18:26:48 -0600

    redesigned he navbar (was too big)

--- frontend/src/app/components/WTitle1.tsx
+++ frontend/src/app/components/WTitle1.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 interface Title1Props {
   text: String;
@@ -12,14 +9,13 @@ interface Title1Props {
 export default function WTitle1({ text, redText }: Title1Props) {
   return (
     <Typography
-      variant="h1"
+      variant="h3"
       sx={{
         color: 'white',
         fontWeight: 'bold',
-        paddingTop: '4rem',
-        paddingBottom: '3rem',
+        // paddingTop: '4rem',
+        // paddingBottom: '3rem',
       }}
-      className={`${inter.className} testing`}
     >
       {text}
       <span>{redText}</span>
