7f78cd8a3d6af68480af22b74f319b7037c5f4f1
Nicolas <nico03aguirre@gmail.com>
2023-11-28 13:22:37 -0600

    Refactor API authorization in service files

--- frontend/src/services/dashboardService.ts
+++ frontend/src/services/dashboardService.ts
@@ -1,12 +1,6 @@
 export const fetchDashboard = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/dashboard`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/dashboard`);
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
--- frontend/src/services/usersService.ts
+++ frontend/src/services/usersService.ts
@@ -1,13 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const fetchUsers = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`);
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -36,13 +30,7 @@ export const fetchUsers = async () => {
 
 export async function fetchUser(userId:string) {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/${userId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/${userId}`);
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -74,10 +62,6 @@ export async function updateUserAdminStatus(userId:string, isAdmin:boolean) {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/admin`, {
       method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
       body: JSON.stringify({
         userId,
         isAdmin,
@@ -114,10 +98,6 @@ export const fetchUserGroups = async (userId:string) => {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/${userId}/groups`, {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
     });
 
     if (!response.ok) {
