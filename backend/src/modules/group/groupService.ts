9c01e0bf5f0d6304fc5d8dc0440c2eb5d5078b66
Pablo <pabloerhard02@gmail.com>
2023-11-29 09:05:11 -0600

    token service

--- backend/src/modules/group/groupService.ts
+++ backend/src/modules/group/groupService.ts
@@ -2,6 +2,7 @@ import groupRepository from './groupRepository';
 import CustomError from '../../utils/errorModel';
 import tokenRepository from '../token/tokenRepository';
 import { CreateTokenDto } from '../token/tokenModel';
+import userRepository from '../user/userRepository';
 
 export const groupService = {
   async getTotalTokensForGroup(): Promise<any> {
@@ -52,20 +53,25 @@ export const groupService = {
       const now = new Date();
       await Promise.all(
         users.map(async user => {
-          const tokenDto: CreateTokenDto = {
-            userId: user.id,
-            amount,
-            currentAmount: amount,
-            expiresAt: new Date(
-              now.getFullYear(),
-              now.getMonth() + 1,
-              1,
-              23,
-              58,
-              59,
-            ),
-          };
-          await tokenRepository.createToken(tokenDto);
+          const token = await userRepository.getUserCurrentToken(user.id);
+          if (token) {
+            await tokenRepository.addTokensToUser(String(token.id), amount);
+          } else {
+            const tokenDto: CreateTokenDto = {
+              userId: user.id,
+              amount,
+              currentAmount: amount,
+              expiresAt: new Date(
+                now.getFullYear(),
+                now.getMonth() + 1,
+                1,
+                23,
+                58,
+                59,
+              ),
+            };
+            await tokenRepository.createToken(tokenDto);
+          }
         }),
       );
     } catch (error) {
